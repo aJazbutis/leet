7ca0207373ed7d03ae4d302e71d983f8836697cb
Arvydas Jazbutis <arvydas.jazbutis@gmail.com>
2024-03-26 13:24:19 +0100

    first missing positive (hard)

---
+++ 41.First_missing_positive/solution.go
@@ -0,0 +1,24 @@
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	for i := range nums {
+		if nums[i] <= 0 || nums[i] > n {
+			nums[i] = n + 1
+		}
+	}
+
+	for i := range nums {
+		if int(math.Abs(float64(nums[i]))) < n+1 {
+			fmt.Println(nums[i], int(math.Abs(float64(nums[i])))-1)
+			if nums[int(math.Abs(float64(nums[i])))-1] > 0 {
+				nums[int(math.Abs(float64(nums[i])))-1] *= -1
+			}
+		}
+	}
+	for i := range nums {
+		if nums[i] > 0 {
+			return i + 1
+		}
+	}
+	return n + 1
+}
+
