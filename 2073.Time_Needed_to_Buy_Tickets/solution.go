2646d0c1f2599fab9544377a071ab454bcf449d2
Arvydas Jazbutis <arvydas.jazbutis@gmail.com>
2024-04-09 12:30:17 +0200

    2073. Time Needed to Buy Tickets (easy)

--- 2073.Time_Needed_to_Buy_Tickets/solution.go
+++ 2073.Time_Needed_to_Buy_Tickets/solution.go
@@ -1,5 +1,26 @@
 func timeRequiredToBuy(tickets []int, k int) int {
 	c := 0
+	for i := range tickets {
+		if i <= k {
+			if tickets[i] <= tickets[k] {
+				c += tickets[i]
+			} else {
+				c += tickets[k]
+			}
+		} else {
+			if tickets[i] < tickets[k] {
+				c += tickets[i]
+			} else {
+				c += tickets[k] - 1
+			}
+		}
+	}
+	return c
+}
+
+//brute as always inital :D
+func timeRequiredToBuy(tickets []int, k int) int {
+	c := 0
 	for {
 		for i := range tickets {
 			if tickets[i] == 0 {
